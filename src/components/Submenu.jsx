60c57c2f598e62f44995ab1d8e44894e9ca85fe2
מוני ברוך <monybaruch@gmail.com>
2023-09-29 05:01:23 +0300

    finishing the submenu jsx and css

--- src/components/Submenu.jsx
+++ src/components/Submenu.jsx
@@ -1,4 +1,26 @@
+import { useGlobalContext } from '../context/contex';
+import sublinks from '../data';
 const Submenu = () => {
-  return <div>Submenu</div>;
+  const { pageId } = useGlobalContext();
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
+  return (
+    <div className={currentPage ? 'submenu show-submenu' : 'submenu'}>
+      <h5>{currentPage?.page}</h5>
+      <div
+        className="submenu-links"
+        style={{ gridTemplateColumns: currentPage?.links?.length > 3 ? '1fr 1fr' : '1fr' }}
+      >
+        {currentPage?.links?.map((link) => {
+          const { id, url, label, icon } = link;
+          return (
+            <a key={id} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Submenu;
